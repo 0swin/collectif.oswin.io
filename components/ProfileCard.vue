aa6b7a5dc36b0566ef37d6dacc5f8c8a0392b0d3
oswin <oswin@oswin.mbp>
2019-10-26 11:29:46 +0200

    First commit

---
+++ components/ProfileCard.vue
@@ -0,0 +1,58 @@
+<template>
+  <div class="p-2">
+    <div class="flex flex-col md:flex-row p-4 bg-white rounded-lg shadow-xl">
+      <div class="flex-grow-0 flex-shrink-0 mr-4">
+        <img
+          class="w-20 lg:w-32 lg:m-2 rounded-full shadow-inner mx-auto"
+          :src="profile.photo"
+          :alt="'Picture of ' + profile.name"
+        />
+      </div>
+      <div class="flex-1 text-gray-900 text-center md:text-left">
+        <h1 class="text-2xl">{{ profile.name }}</h1>
+        <p class="text-sm text-gray-700 uppercase tracking-wide">{{ profile.job }}</p>
+        <hr class="my-2 mr-4" />
+        <ul>
+          <li class="mb-1" v-for="skill in profile.skills" :key="skill.id">
+            <div class="inline-block h-5 w-5 rounded-full text-blue-500 align-text-bottom">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                stroke="currentColor"
+                stroke-width="2"
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                class="feather feather-check-circle"
+                viewBox="0 0 24 24"
+              >
+                <path d="M22 11v1a10 10 0 11-6-9" />
+                <path d="M22 4L12 14l-3-3" />
+              </svg>
+            </div>
+            {{ skill }}
+          </li>
+        </ul>
+        <hr class="my-2 mx-6" />
+        <ul>
+          <li class="inline mr-4" v-for="link in profile.links" :key="link.id">
+            <a
+              class="text-sm uppercase tracking-wide text-blue-600 underline"
+              :href="link.url"
+            >{{ link.text }}</a>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    profile: Object
+  }
+};
+</script>
+
+<style>
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,91 @@
+<template>
+  <div class="max-w-6xl mx-auto px-4">
+    <h1 class="text-4xl text-center font-bold uppercase my-4">oswin.io</h1>
+    <div class="flex flex-row flex-wrap">
+      <ProfileCard
+        class="w-full md:w-1/2"
+        v-for="member in members"
+        :key="member.id"
+        :profile="member.profile"
+      />
+    </div>
+  </div>
+</template>
+
+<script>
+import ProfileCard from "@/components/ProfileCard";
+
+export default {
+  components: {
+    ProfileCard
+  },
+  data() {
+    return {
+      members: {
+        NicolasRoger: {
+          profile: {
+            name: "Nicolas Roger",
+            job: "Creative Technologist @ Blackfoot.io",
+            photo: "assets/jpg/nicolasroger.jpg",
+            skills: {
+              1: "Développement frond-end",
+              2: "Modélisation 3D",
+              3: "Photographie"
+            },
+            links: {
+              instagram: {
+                text: "instagram",
+                url: "https://instagram.com/0sw.in"
+              },
+              linkedin: {
+                text: "linkedin",
+                url: "https://linkedin.com/nicolas9roger"
+              },
+              github: {
+                text: "github",
+                url: "https://github.com/0swin"
+              },
+              mail: {
+                text: "mail",
+                url: "mailto:nicolas@oswin.io"
+              }
+            }
+          }
+        },
+        MelissaLuntadila: {
+          profile: {
+            name: "Mélissa Luntadila",
+            job: "Graphic Designer",
+            photo: "assets/jpg/melissaluntadila.jpg",
+            skills: {
+              1: "Identité graphique",
+              2: "Illustration digitale",
+              3: "Animation frame by frame"
+            },
+            links: {
+              instagram: {
+                text: "instagram",
+                url: "https://instagram.com/melilunt"
+              },
+              linkedin: {
+                text: "linkedin",
+                url: "https://linkedin.com/in/melilun"
+              },
+              web: {
+                text: "web",
+                url: "https://melilun.ml"
+              }
+            }
+          }
+        }
+      }
+    };
+  }
+};
+</script>
+
+<style>
+body {
+  @apply bg-gray-300;
+}
+</style>
