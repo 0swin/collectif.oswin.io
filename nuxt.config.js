32afc50265a21e79d8dfab51ec5dc9b528d3074f
Nicolas "0swin" Roger <nicolas@oswin.io>
2019-12-24 15:48:30 +0100

    Nuxt config for Zeit Now

--- nuxt.config.js
+++ nuxt.config.js
@@ -18,6 +18,9 @@ export default {
     ],
     link: [{ rel: "icon", type: "image/x-icon", href: "/favicon.ico" }],
   },
+  "generate": {
+    "dir": "public"
+  },
   /*
    ** Customize the progress-bar color
    */
